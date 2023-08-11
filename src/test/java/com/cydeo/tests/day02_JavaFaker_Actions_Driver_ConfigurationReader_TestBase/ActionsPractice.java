ca71b7f1ccd46115cdb879baa410891855a2600b
joskyday <joskyday@gmail.com>
2023-08-11 17:18:20 -0400

    day03

--- src/test/java/com/cydeo/tests/day02_JavaFaker_Actions_Driver_ConfigurationReader_TestBase/ActionsPractice.java
+++ src/test/java/com/cydeo/tests/day02_JavaFaker_Actions_Driver_ConfigurationReader_TestBase/ActionsPractice.java
@@ -3,21 +3,64 @@ package com.cydeo.tests.day02_JavaFaker_Actions_Driver_ConfigurationReader_TestB
 import com.cydeo.utilities.Driver;
 import com.cydeo.utilities.TestBase;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class ActionsPractice extends TestBase {
+
+    WebDriver driver=Driver.get();
     @Test
     public void hoverTest(){
-        //click home
-        Driver.get().findElement(By.xpath("//a[@class='nav-link']")).click();
-        //goto hover link
-        //locator of the hover   //a[@href='/hovers']
-        Driver.get().findElement(By.xpath("//a[@href='/hovers']")).click();
-
-
-
+       //goto hovers URL
+        //https://practice.cydeo.com/hovers
+        driver.get("https://practice.cydeo.com/hovers");
+// locate the first image and hover over the image
+        WebElement image1=driver.findElement(By.xpath("(//img[@alt='User Avatar'])[1]"));
+        actions.moveToElement(image1).perform();
+        //locate name: user1 element and verify it is displayed using  isDisplayed() method
+        WebElement user1=Driver.get().findElement(By.xpath("//h5[text()='name: user1']"));
+        //verify user1 name is dsiplayed
+        Assert.assertTrue(user1.isDisplayed(),"There is problem in the web page source code");
 
     }
+
+    @Test
+    public void circleTest(){
+        //goto https://practice.cydeo.com/drag_and_drop_circles
+
+        driver.get("https://practice.cydeo.com/drag_and_drop_circles");
+
+        //locate draggable circle
+        WebElement smallCircle=driver.findElement(By.xpath("//div[@id='draggable']"));
+        actions.clickAndHold(smallCircle).moveToElement(smallCircle,100,0).pause(Duration.ofSeconds(3)).perform();
+        //  locate the big circle text saying Drop here.
+        WebElement dropHere= driver.findElement(By.xpath("//div[text()='Drop here.']"));
+        //verify that when clickandhold and move the small circle it says Drop here in big circle.
+        Assert.assertTrue(dropHere.isDisplayed(),"There is a problem");
+
+    }
+    @Test
+    public void circleTestTryAgain(){
+        //goto https://practice.cydeo.com/drag_and_drop_circles
+
+        driver.get("https://practice.cydeo.com/drag_and_drop_circles");
+
+        //locate draggable circle
+        WebElement smallCircle=driver.findElement(By.xpath("//div[@id='draggable']"));
+        //move the small circle and release
+        actions.clickAndHold(smallCircle).moveToElement(smallCircle,100,0).release().perform();
+        //  locate the big circle text saying Drop here.
+        WebElement bigCircle= driver.findElement(By.xpath("//div[@id='droptarget']"));
+        //verify that when clickandhold and move the small circle it says Drop here in big circle.
+        String expected="Try again!";
+        Assert.assertTrue(bigCircle.getText().equals(expected),"There is a problem");
+
+    }
+
 
 
 }
--- src/test/java/com/cydeo/tests/day02_JavaFaker_Actions_Driver_ConfigurationReader_TestBase/FillRegistrationForm.java
+++ src/test/java/com/cydeo/tests/day02_JavaFaker_Actions_Driver_ConfigurationReader_TestBase/FillRegistrationForm.java
@@ -5,12 +5,18 @@ import com.cydeo.utilities.Driver;
 import com.cydeo.utilities.TestBase;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class FillRegistrationForm  extends TestBase {
+
+    WebDriver driver=Driver.get();
 
     @Test
     public void registerFormTest() throws InterruptedException {
@@ -32,6 +38,11 @@ public class FillRegistrationForm  extends TestBase {
         WebElement lastName=Driver.get().findElement(By.xpath("//input[@name='lastname']"));
         lastName.sendKeys(lastname);
 
+        //to create a random fake username
+        String username=fake.bothify(firstname+"###???");  //## for numbers  ? is for letters
+        WebElement userName = Driver.get().findElement(By.xpath("//input[@name='username']"));
+        userName.sendKeys(username);
+
         //fill email
         String email=fake.internet().emailAddress();
         WebElement emailAddress=Driver.get().findElement(By.xpath("//input[@name='email']"));
@@ -40,7 +51,54 @@ emailAddress.sendKeys(email);
 String phone=fake.numerify("###-###-####");
 WebElement phoneNumber=Driver.get().findElement(By.xpath("//input[@name='phone']"));
         phoneNumber.sendKeys(phone);
-        Thread.sleep(5000);
-    }
+        // fill password using fake.internet().password()
+        String password=fake.internet().password();
+        System.out.println("password = " + password);
+        WebElement passWord=driver.findElement(By.xpath("//input[@name='password']"));
+        passWord.sendKeys(password);
+        //select gender according to random gender input you get from
+        //fake.dog().gender()
+        //so locate gender radio button
+        // create a dynamic locator by using gender variable inside the locator
+        // so for male, locator will point male
+        //for female, locator will point female
+        //such as String gender=male; ......By.xpath("//a[@id='" +   gender  +  "']");
+        String gender=fake.dog().gender();
+        WebElement genderButton=Driver.get().findElement(By.xpath("//input[@value='"+   gender     +"']"));
+        genderButton.click();
+        // create a fake birthday filling this format
+        //MM/DD/YYYY
+        //use this code
+        //SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        //String dob = sdf.format(fake.date().birthday());
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        String dob = sdf.format(fake.date().birthday());
+        WebElement birthday=driver.findElement(By.xpath("//input[@name='birthday']"));
+        birthday.sendKeys(dob);
 
+        //Select Treasurer's Office using selectByValue() from Department/Office dropdown
+        Select departmentDropdown=new Select(Driver.get().findElement(By.xpath("//select[@name='department']")));
+        departmentDropdown.selectByValue("TRO");
+        //Select SDET using selectByIndex() from Job title dropdown
+        Select jobTitleDropdown=new Select(Driver.get().findElement(By.xpath("//select[@name='job_title']")));
+        jobTitleDropdown.selectByIndex(4);
+        //Select C++, Java and Javascript all 3 checkboxes
+        WebElement cplusplus=    Driver.get().findElement(By.xpath("//input[@id='inlineCheckbox1']"));
+        cplusplus.click();
+
+        WebElement java=Driver.get().findElement(By.xpath("//input[@id='inlineCheckbox2']"));
+        java.click();
+
+        WebElement javascript=Driver.get().findElement(By.xpath("//input[@id='inlineCheckbox3']"));
+        javascript.click();
+
+        //click signup
+        Driver.get().findElement(By.xpath("//button[@id='wooden_spoon']")).click();
+
+        //verify "Well done!" is shown to show succesful signup after clicking signup button.
+        String expectedText="Well done!";
+        WebElement WellDone=Driver.get().findElement(By.xpath("//h4[text()='Well done!']"));
+
+        Assert.assertEquals(expectedText,WellDone.getText(),"Expected text is shown");
+    }
 }
